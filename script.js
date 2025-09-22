968101696267e658de95d7c37b689d00736bf7f9
Hrishikesh Alabnur <144805491+RishiDevs3248@users.noreply.github.com>
2025-09-22 11:29:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-//your JS code here. If required.
+let boxes = document.querySelectorAll('.square');
+
+boxes.forEach(box => {
+    box.addEventListener("mouseover", () => {
+        boxes.forEach(b => {
+            b.style.backgroundColor = "#6F4E37";
+        });
+		box.style.backgroundColor = "#E6E6FA";
+    });
+    box.addEventListener("mouseout", () => {
+        boxes.forEach(b => {
+            b.style.backgroundColor = "#E6E6FA";
+        });
+    });
+	
+});
